c1a24612c24b60011fcdffb02c71bfd8aa8b7347
Gaelle Shearn <gaelle.shearn.jmr@gmail.com>
2020-11-17 19:18:03 +0100

    I can view all the chompignons I created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'mushrooms#index'
+    get '/mushrooms/mymushrooms', to: 'mushrooms#show_my', as: 'my_mushrooms'
     resources :mushrooms, only: [:index, :show, :create, :new, :update, :edit]
+
 end
