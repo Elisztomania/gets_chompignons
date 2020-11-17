4d8e4207d3e43439213f4b280287dffc30b4c66d
Elise <elisztomania@gmail.com>
2020-11-17 15:00:37 +0100

    collection de locations

--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -2,10 +2,11 @@ class Mushroom < ApplicationRecord
   belongs_to :user
 
   CAP = %w(Convexe Entonnoir Globuleux Plat Déprimé Mamelonné)
+  LOCATIONS = ['Bois de feuillus', 'Bois de conifères', 'Bois de feuillus et conifères', 'Lisières', 'Prés', 'Souches', 'Mousse', 'Bois pourrissant', 'Sol pas ou peu calcaire', 'Sol calcaire']
 
   validates :name, presence: true, uniqueness: true
   validates :content, presence: true
-  validates :location, presence: true
+  validates :location, presence: true, inclusion: { in: LOCATIONS }
   validates :cap, inclusion: { in: CAP }
   validates :user, presence: true
 end
