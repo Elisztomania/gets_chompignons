49c011ec71ac834e33e31a140e5d9257c63cb1b9
Gaelle Shearn <gaelle.shearn.jmr@gmail.com>
2020-11-17 11:20:14 +0100

    qucik modif

--- app/controllers/mushrooms_controller.rb
+++ app/controllers/mushrooms_controller.rb
@@ -1,4 +1,5 @@
 class MushroomsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :set_mushroom, only: [:show, :edit, :update]
 
   def index
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :index
 
-  def home
+  def index
   end
 end
