d4265ac2b124e09abe284a8d77d06c38bb18b17f
Elise <elisztomania@gmail.com>
2020-11-17 11:44:25 +0100

    saving unsaved file

--- app/controllers/mushrooms_controller.rb
+++ app/controllers/mushrooms_controller.rb
@@ -1,4 +1,4 @@
-class MushroomsController < ApplicationController
+heclass MushroomsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_mushroom, only: [:show, :edit, :update]
 
