292c78a599154a027b807c195044aedc6c66d7f7
Gagou <gaelle.shearn.jmr@gmail.com>
2020-11-18 20:22:24 +0100

    Review new column user_id reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_113638) do
+ActiveRecord::Schema.define(version: 2020_11_18_192125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,9 @@ ActiveRecord::Schema.define(version: 2020_11_18_113638) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "mushroom_id", null: false
+    t.bigint "user_id", null: false
     t.index ["mushroom_id"], name: "index_reviews_on_mushroom_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -84,4 +86,5 @@ ActiveRecord::Schema.define(version: 2020_11_18_113638) do
   add_foreign_key "favorites", "users"
   add_foreign_key "mushrooms", "users"
   add_foreign_key "reviews", "mushrooms"
+  add_foreign_key "reviews", "users"
 end
