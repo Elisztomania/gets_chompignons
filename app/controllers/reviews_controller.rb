1678cd42853289cd3056650794d1672c357ce303
Gagou <gaelle.shearn.jmr@gmail.com>
2020-11-18 19:17:53 +0100

    show qui tient plus la route

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_mushroom, only: [:new, :create, :edit]
+  before_action :set_mushroom, only: [:new, :create]
 
   def new
     @review = Review.new
@@ -16,11 +16,15 @@ class ReviewsController < ApplicationController
   end
 
   def edit
+    @mushroom = Mushroom.find(params[:id])
+    @review = Review.find(params[:mushroom_id])
   end
 
   def update
+    @mushroom = Mushroom.find(params[:mushroom_id])
+    @review = Review.find(params[:id])
     if @review.update(review_params)
-      redirect_to mushroom_path(@mushroom), notice: 'Votre avis a été correctement modifié.'
+      redirect_to @mushroom, notice: 'Votre avis a été correctement modifié.'
     else
       render :edit
     end
