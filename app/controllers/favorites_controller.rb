0c04551e745fc2f7f79287cee6170824e5dd5938
Elise <elisztomania@gmail.com>
2020-11-18 13:38:50 +0100

    changing methods

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,21 @@
 class FavoritesController < ApplicationController
+  before_action :set_mushroom, only: [:create, :destroy]
 
-  def index
-    @favorite = Mushroom.find(params[:id])
+  def create
   end
 
+  def destroy
+  end
 
+  def show
+    @mushrooms = Mushroom.all
+    @user_id = current_user.id
+    @favorites = Mushroom.where(user_id: @user_id)
+  end
+
+  private
+
+  def set_mushroom
+    @mushroom = Mushroom.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'mushrooms#index'
     get '/mushrooms/mymushrooms', to: 'mushrooms#show_my', as: 'my_mushrooms'
     get '/favorites', to: 'favorites#show'
-    resources :mushrooms, only: [:index, :show, :create, :new, :update, :edit]
-
+    resources :mushrooms, only: [:index, :show, :create, :new, :update, :edit] do
+      resources :favorites, only: [:create, :new]
+    end
+    resources :favorites; only: :destroy
 end
