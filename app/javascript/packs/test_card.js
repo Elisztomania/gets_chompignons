82f976dc81472118db1545b74378010a16c22de0
Huy Vu	Nguyen <hv.ng@outlook.fr>
2020-11-19 11:54:05 +0100

    edit component test card  chompignon

--- app/javascript/packs/test_card.js
+++ app/javascript/packs/test_card.js
@@ -1,77 +1,34 @@
-Vue.config.devtools = true;
+// This is "probably" IE9 compatible but will need some fallbacks for IE8
+// - (event listeners, forEach loop)
 
-Vue.component('card', {
-  template: `
-    <div class="card-wrap"
-      @mousemove="handleMouseMove"
-      @mouseenter="handleMouseEnter"
-      @mouseleave="handleMouseLeave"
-      ref="card">
-      <div class="card"
-        :style="cardStyle">
-        <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
-        <div class="card-info">
-          <slot name="header"></slot>
-          <slot name="content"></slot>
-        </div>
-      </div>
-    </div>`,
-  mounted() {
-    this.width = this.$refs.card.offsetWidth;
-    this.height = this.$refs.card.offsetHeight;
-  },
-  props: ['dataImage'],
-  data: () => ({
-    width: 0,
-    height: 0,
-    mouseX: 0,
-    mouseY: 0,
-    mouseLeaveDelay: null
-  }),
-  computed: {
-    mousePX() {
-      return this.mouseX / this.width;
-    },
-    mousePY() {
-      return this.mouseY / this.height;
-    },
-    cardStyle() {
-      const rX = this.mousePX * 30;
-      const rY = this.mousePY * -30;
-      return {
-        transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
-      };
-    },
-    cardBgTransform() {
-      const tX = this.mousePX * -40;
-      const tY = this.mousePY * -40;
-      return {
-        transform: `translateX(${tX}px) translateY(${tY}px)`
-      }
-    },
-    cardBgImage() {
-      return {
-        backgroundImage: `url(${this.dataImage})`
-      }
-    }
-  },
-  methods: {
-    handleMouseMove(e) {
-      this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width/2;
-      this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height/2;
-    },
-    handleMouseEnter() {
-      clearTimeout(this.mouseLeaveDelay);
-    },
-    handleMouseLeave() {
-      this.mouseLeaveDelay = setTimeout(()=>{
-        this.mouseX = 0;
-        this.mouseY = 0;
-      }, 1000);
-    }
-  }
+// wait for the entire page to finish loading
+window.addEventListener('load', function() {
+
+  // setTimeout to simulate the delay from a real page load
+  setTimeout(lazyLoad, 1000);
+
 });
 
-const app = new Vue({
-  el: '#app'
-});
+function lazyLoad() {
+  var card_images = document.querySelectorAll('.card-image');
+
+  // loop over each card image
+  card_images.forEach(function(card_image) {
+    var image_url = card_image.getAttribute('data-image-full');
+    var content_image = card_image.querySelector('img');
+
+    // change the src of the content image to load the new high res photo
+    content_image.src = image_url;
+
+    // listen for load event when the new photo is finished loading
+    content_image.addEventListener('load', function() {
+      // swap out the visible background image with the new fully downloaded photo
+      card_image.style.backgroundImage = 'url(' + image_url + ')';
+      // add a class to remove the blur filter to smoothly transition the image change
+      card_image.className = card_image.className + ' is-loaded';
+    });
+
+  });
+
+}
+
